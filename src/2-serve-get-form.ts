6be7662f2500d619cc5528f07d4c9fbc23d8fd08
bunny <13199000154@qq.com>
2022-10-09 00:55:01 +0800

    2022-10-9-serve

---
+++ src/2-serve-get-form.ts
@@ -0,0 +1,17 @@
+// /*get获取表单数据*/
+// const http = require("http");
+// const url = require("url");
+// const queryString = require("querystring");
+// const port = 5500;
+// const serve = http.createServer((req: any, res: any) => {
+//   //  防止乱码
+//   res.writeHead(200, { "Content-Type": "text/html;charset=utf8" });
+//   const reqUrl = req.url;
+//   //
+//   const formVal = url.parse(reqUrl, true).query;
+//   console.log(formVal.useName, formVal.usePwd);
+//   // res.end();
+//   res.end(`用户名:${formVal.useName},密码:${formVal.usePwd}`);
+// });
+// serve.listen(port);
+// console.log(`${port} list......`);
